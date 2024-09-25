fc9e8edcc3ba47b9b81b1a20ca4ea3a41aba98e7
Paweł Sroka <pawel.sroka0@gmail.com>
2024-09-25 18:58:18 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="A package with data related helper functions.",
     name="pytoolkit",
     packages=find_packages(include=["pytoolkit", "pytoolkit.*"]),
-    install_requires=['numpy>=1.22', 'pandas>=1.5', 'seaborn>=0.12.0', 'scikit-learn>=1.1.0', 'matplotlib>=3.4.0', 'plotnine>=0.12.1, plotly>=5.15.0'],
+    install_requires=['numpy>=1.22', 'pandas>=1.5', 'seaborn>=0.12.0', 'scikit-learn>=1.1.0', 'matplotlib>=3.4.0', 'plotnine>=0.12.1', 'plotly>=5.15.0'],
     python_requires='>=3.8.10',
     version="0.1.0"
 )
