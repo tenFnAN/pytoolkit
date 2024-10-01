28021348bea1dbb230c7a18793e750cabf019012
Paweł Sroka <pawel.sroka0@gmail.com>
2024-10-01 10:27:01 +0200

    add box

--- pytoolkit/pytoolkit.py
+++ pytoolkit/pytoolkit.py
@@ -683,7 +683,54 @@ def kit_squishToRange(series, lower_percentile=0.01, upper_percentile=0.99):
     upper_bound = series.quantile(upper_percentile)
     return series.clip(lower=lower_bound, upper=upper_bound)
 
+
+## plot
+def draw_boxplot_num(data, y, title="Box Plot"):
+
+    boxplot = (
+        ggplot.ggplot(data, ggplot.aes(y=y)) +  # Specify x and y variables
+        ggplot.geom_boxplot() +                      # Add the box plot geometry
+        ggplot.labs(title=title) +
+        ggplot.coord_flip()                  
+    )
+    
+    return boxplot
+
+def draw_boxplot_cat(data, by, y, title="Box Plot"):
+
+    if data[by].dtype not in ['object','category', 'string', 'bool']:
+        data[by] = data[by].astype('object')
+ 
+    boxplot = (
+        ggplot.ggplot(data) + 
+        ggplot.geom_boxplot(ggplot.aes(x=by, y=y)) +                      
+        ggplot.labs(title=title)              
+    )
+    
+    return boxplot
+
+def draw_boxplot_all(data, ncol = 3):
+    # Reshape the DataFrame from wide to long format
+    data_long = pd.melt(data[my.num_vars(data)])
+
+    # Create the boxplot
+    p = (
+        ggplot.ggplot(data_long, ggplot.aes(x='variable', y='value')) +
+        ggplot.geom_boxplot() +
+        ggplot.facet_wrap('variable', scales='free', ncol=ncol) +
+        ggplot.theme_bw()
+    )
+
+    return p
+
+
+
+
+
 def reduce_mem_usage(df, verbose=True):
+    """
+    source https://www.kaggle.com/code/arjanso/reducing-dataframe-memory-size-by-65 
+    """
     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     start_mem = df.memory_usage().sum() / 1024**2    
     for col in df.columns:
@@ -710,6 +757,51 @@ def reduce_mem_usage(df, verbose=True):
     end_mem = df.memory_usage().sum() / 1024**2
     if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
     return df
+
+def reduce_mem_usage_pl(df):
+        """ Reduce memory usage by polars dataframe {df} with name {name} by changing its data types. 
+            source https://www.kaggle.com/code/yuuniekiri/fork-of-home-credit-catboost-inference
+        """
+        print(f"Memory usage of dataframe is {round(df.estimated_size('mb'), 2)} MB")
+        
+        Numeric_Int_types = [pl.Int8, pl.Int16, pl.Int32, pl.Int64]
+        Numeric_Float_types = [pl.Float32, pl.Float64]    
+        
+        for col in df.columns:
+            if col == 'case_id': 
+                continue
+            try:
+                col_type = df[col].dtype
+                
+                if col_type == pl.Categorical:
+                    continue
+                    
+                c_min = df[col].min()
+                c_max = df[col].max()
+                
+                if col_type in Numeric_Int_types:
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df = df.with_columns(df[col].cast(pl.Int8))
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df = df.with_columns(df[col].cast(pl.Int16))
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df = df.with_columns(df[col].cast(pl.Int32))
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df = df.with_columns(df[col].cast(pl.Int64))
+                
+                elif col_type in Numeric_Float_types:
+                    if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df = df.with_columns(df[col].cast(pl.Float32))
+                    else:
+                        pass
+                # elif col_type == pl.Utf8:
+                #     df = df.with_columns(df[col].cast(pl.Categorical))
+                else:
+                    pass
+            except:
+                pass
+        print(f"Memory usage of dataframe became {round(df.estimated_size('mb'), 2)} MB")
+        return df
 
 def get_memory_usage():
     return np.round(psutil.Process(os.getpid()).memory_info()[0]/2.**30, 2) 
